03971bda4ff425ed7d6adef15594edc39dd04a52
Peter BALIVET <peterbalivet@gmail.com>
2022-04-22 23:17:22 +0200

    added node to vuln-scanner

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-apt update
-apt install git -y
+#apt update
+#apt install git -y
 
 git clone https://github.com/cve-search/CVE-Search-Docker.git
 git clone https://github.com/vulsio/go-exploitdb.git
