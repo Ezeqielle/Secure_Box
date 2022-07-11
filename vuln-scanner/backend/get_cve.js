243d5487c8a0c4c2eb9e761c60647d75ab21e8ba
hxtninfosec <hxtninfosec@pop-os.localdomain>
2022-07-11 14:34:36 +0200

    Get CVE for a product

--- vuln-scanner/backend/get_cve.js
+++ vuln-scanner/backend/get_cve.js
@@ -9,11 +9,62 @@ const uri = `mongodb://${hostname}:${port}/?authSource=admin`;
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
-async function readDataInDb() {
+async function getCVE(prmProduct, prmVendor) {
     try {
         const db = client.db("cvedb");
-        const collection = db.collection("users");
-        const result = await collection.find({}).toArray();
+        const collection = db.collection("cpe");
+        const result = await collection.aggregate([
+            {
+                $unwind: "$cpe_name"
+            },
+            {
+                $match: {
+                    $or: [{
+                        product: /openssh/
+                    },
+                    {
+                        vendor: /openssh/
+                    }],
+                    $and: [{
+                        $or: [{
+                            "cpe_2_2": /:5.8:/
+                        },
+                        {
+                            "cpe_name.cpe23Uri": /:5.8:/
+                        }]
+                    }, 
+                    {
+                        //cpe_2_2: //TO WORK ON
+                    }]
+                }
+            }, 
+            {
+                $project: {
+                    "_id": 0,
+                    "cpe_2_2": 1,
+                    "cpe_name.cpe23Uri": 1
+                }
+            },
+            {
+                $group: {
+                    _id: "$cpe_name.cpe23Uri"
+                }
+            },
+            {
+                $lookup: {
+                    from: "cves",
+                    localField: "_id",
+                    foreignField: "vulnerable_configuration",
+                    as: "cves"
+                }
+            },
+            {
+                $unwind: "$cves"
+            },
+            {
+                $group: { _id: "$cves.id", data: {$first: "$cves"}}
+            }
+        ]).toArray();
         console.log(result);
     } catch {
         console.error("Error reading data from DB");
@@ -27,7 +78,7 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Connected successfully to Mongo");
 
-    await readDataInDb();
+    await getCVE("openssh", "openssh");
   } catch {
     console.error("Error connecting to Mongo");
   } finally {
