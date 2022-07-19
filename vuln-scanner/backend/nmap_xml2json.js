51b3f88c465d1b98d3ea9fee03c4a1e40f989fa4
Mathis DI MASCIO <mathis.dimascio@gmail.com>
2022-07-19 15:09:43 +0200

    command exec

---
+++ vuln-scanner/backend/nmap_xml2json.js
@@ -0,0 +1,29 @@
+const { exec } = require("child_process");
+
+function execCode(repport, ipRange) {
+
+    let cmd = "nmap -v -sS -p- -sV 9 -PR -O -oX res.xml " + ipRange;
+    exec(cmd, (error, stdout,stderr) => {
+        if(error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if(stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+    
+    exec("fxparser res.xml > result.json", (error, stdout,stderr) => {
+        if(error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if(stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
