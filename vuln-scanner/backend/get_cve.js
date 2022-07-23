b745704692d1768e0effed2770364b3783bd2096
hxtninfosec <hxtninfosec@gmail.com>
2022-07-23 20:43:13 +0200

    Upload to database

--- vuln-scanner/backend/get_cve.js
+++ vuln-scanner/backend/get_cve.js
@@ -1,18 +1,52 @@
 const { MongoClient } = require("mongodb");
 const { fs } = require("fs");
+const { REPL_MODE_STRICT } = require("repl");
+const mysql = require('mysql');
+const { json } = require("express");
 // Connection URI
 
 const hostname = "10.21.22.8";
 const port = "27017";
 const uri = `mongodb://${hostname}:${port}/?authSource=admin`;
 
-let output;
-
-
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
-async function getCVE(prmProduct, prmVersion) {
+async function uploadToDatabase(prmPortId, prmCVES) {
+    const db = mysql.createConnection({
+        host: "10.21.22.4",
+        user: "root",
+        password: "example",
+        database: "secureBox"
+    });
+
+
+    db.connect(function (err) {
+        if (err) throw err;
+        console.log("Connecté à la base de données MySQL!");
+    });
+
+    const obj = JSON.parse(prmCVES);
+    for (let cveIndex = 0; cveIndex < Object.keys(obj).length; cveIndex++){
+        let cve = obj[cveIndex]["data"];
+        const references = JSON.stringify(Object.assign({}, cve["references"]))
+
+        db.query(`INSERT INTO CVE (cve_name, cve_published, cve_summary, cve_access_auth, cve_access_complexity, cve_access_vector, cve_impact_avaibility, cve_impact_confidentiality, cve_impact_integrity, cve_cvss_three, cve_cvss, cve_exploitability_score, cve_impact_score, cve_references, cve_cwe, port_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`, [
+        cve['id'], cve['Published'], cve['summary'], cve["access"]["authentication"], cve["access"]["complexity"],
+        cve["access"]["vector"], cve["impact"]["availability"], cve["impact"]["confidentiality"], cve["impact"]["integrity"],
+        cve["cvss3"], cve["cvss"], cve["exploitabilityScore"], cve["impactScore"], references, cve["cwe"], prmPortId], (error) => {
+            if (error) throw error;
+        })
+        
+        /* INSERT INTO CVE (cve_name, cve_summary, cve_access_auth, cve_access_complexity, cve_access_vector, cve_impact_avaibility, cve_impact_confidentiality, cve_impact_integrity, cve_cvss_three, cve_cvss, cve_exploitability_score, cve_impact_score, cve_references, cve_cwe, port_id)
+           VALUES ("CVE-0000-0000", "This is a CVE TEST", "NONE", "LOW", "NETWORK", "PARTIAL", "NONE", "NONE", 7.8, 6.9, 10, 2.4, '{"0": "Reference", "1": "Reference"}', "CWE-TEST", 1); */
+
+    }
+    db.end();
+}
+
+async function getCVE(prmProduct, prmVersion, prmService) {
     try {
         const db = client.db("cvedb");
         const collection = db.collection("cpe");
@@ -23,10 +57,10 @@ async function getCVE(prmProduct, prmVersion) {
             {
                 $match: {
                     $or: [{
-                        "cpe_2_2": new RegExp(prmProduct, 'i')
+                        "product": new RegExp(prmProduct, 'i')
                     },
                     {
-                        "cpe_name.cpe23Uri": new RegExp(prmProduct, 'i')
+                        "vendor": new RegExp(prmProduct, 'i')
                     }],
                     $and: [{
                         $or: [{
@@ -35,6 +69,9 @@ async function getCVE(prmProduct, prmVersion) {
                         {
                             "cpe_name.cpe23Uri": new RegExp(':' + prmVersion + ':')
                         }]
+                    }, 
+                    {
+                        "cpe_2_2": new RegExp(prmService, 'i')
                     }]
                 }
             }, 
@@ -65,7 +102,9 @@ async function getCVE(prmProduct, prmVersion) {
                 $group: { _id: "$cves.id", data: {$first: "$cves"}}
             }
         ]).toArray();
-        output = result;
+        data = result
+        dataFormatted = JSON.stringify(data, null, 2);
+        uploadToDatabase("1", dataFormatted);
     } catch {
         console.error("Error reading data from DB");
     }
@@ -78,9 +117,8 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Connected successfully to Mongo");
 
-    await getCVE("apache:http_server", "2.4.1");
-    let dataFormatted = JSON.stringify(output, null, 2);
-    console.log(dataFormatted)
+    
+    await getCVE("apache", "2.4.1", "http");
   } catch {
     console.error("Error connecting to Mongo");
   } finally {
