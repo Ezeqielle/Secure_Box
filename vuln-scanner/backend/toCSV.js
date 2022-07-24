0ef4607f407c4d73ae445e5164fd146851a1cf59
Ezeqielle <mathis.dimascio@gmail.com>
2022-07-24 19:36:08 +0200

    add hash_id for reports

--- vuln-scanner/backend/toCSV.js
+++ vuln-scanner/backend/toCSV.js
@@ -5,8 +5,6 @@ const fs = require("fs");
 // generate id 50 char
 // check if not already in bdd
 
-const ws = fs.createWriteStream("output.csv");
-
 const db = mysql.createConnection({
     host: "10.21.22.4",
     user: "root",
@@ -19,7 +17,38 @@ db.connect(function (err) {
     console.log("Connecté à la base de données MySQL!");
 });
 
+function isInDB(data) {
+
+    if(db.query('SELECT id FROM Scans WHERE hash_id = ?', [data])){
+       return true;
+    } else {
+        return false;
+    }
+}
+
+const generateRandomString = (strLength, scanId) => {
+    const chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+    const randomArray = Array.from({
+        length: strLength
+    },(v, k) => chars[Math.floor(Math.random() * chars.length)]);
+
+    const randomString = randomArray.join("");
+    if(isInDB(randomString)){
+        generateRandomString(strLength, scanId);
+    } else {
+        reportHashID(randomString, scanId);
+    }
+}
+
+const reportHashID = (hashID, scanId) => {
+    db.query('UPDATE Scans SET hash_id = ? WHERE scan_id = ?', [hashID, scanId]);
+    const ws = fs.createWriteStream(`${hashID}.csv`);
+    return ws;
+}
+
+// function to call 
 async function toCSV(scanId) {
+    generateRandomString(50, scanId)
     db.query("SELECT * FROM Hosts WHERE scan_id = ?", [scanId] , function(error, data, fields) {
         if (error) throw error;
         const jsonData = JSON.parse(JSON.stringify(data));
